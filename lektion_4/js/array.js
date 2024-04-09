d2e9df9dfa2d42823cd5fc943e835da7d7456454
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-04-09 12:00:20 +0200

    push lesson 4

--- lektion_4/js/array.js
+++ lektion_4/js/array.js
@@ -1,11 +1,49 @@
-const arr = [2,3,56,75,45,43,22243];
+const arr = [0,2, 3, 56, 75, 45, 43, 22243, -22];
 
-function callBack (el, i, array){
+function callBack(el, i, array) {
     console.log(el, i, array);
     el = 0;
-    array[i] = 0;
+    //array[i] = 0;
 }
 
 arr.forEach(callBack);
 
 console.log(arr);
+
+arr.forEach((el) => {
+    console.log(el);
+});
+
+//filtrera alla nummer som är under 50
+const lessThanFifty = arr.filter((num) => num < 50);
+console.log(lessThanFifty);
+
+//jämna nummer
+const evenNumbers = arr.filter((num) => num % 2 == 0);
+console.log(evenNumbers);
+
+//när filter?
+//filtrera prudukter utifrån pris
+
+//map
+
+const mapArr = arr.map(num => num % 2 == 0);
+console.log(mapArr);
+//när?
+//konvertera enhet till en annan 
+
+
+//sort 
+console.log(arr);
+const stigande = arr.sort((a,b)=>{
+    console.log(a,b);
+    return a-b;
+});
+console.log(stigande);
+
+const fallande = arr.sort((a,b) => b-a)
+console.log(fallande);
+
+//när?
+//rankinglista -> vem sprang snabbast 
+// produkter, filter 
--- lektion_4/js/script.js
+++ lektion_4/js/script.js
@@ -1,24 +1,61 @@
 const obj = {
     x: 10,
-    y: 20
-}
+    y: 20,
+};
 
-function add(objPar, num){
-
-    objPar.x+=num;
+function add(objPar, num) {
+    objPar.x += num;
 }
 
 console.log(obj);
 
-add(obj,3)
+add(obj, 3);
 
 console.log(obj);
 
-const x =10;
-function addPrimitive(xPar,num){
-    xPar+=num;
+const x = 10;
+function addPrimitive(xPar, num) {
+    xPar += num;
 }
 
 console.log(x);
-addPrimitive(x,3);
+addPrimitive(x, 3);
 console.log(x);
+
+//destructing
+const deObj = { a: 1, b: 2 };
+// const a = deObj.a;
+// const b = deObj.b;
+const { a, b } = deObj;
+console.log(a, b);
+
+function addProps({ a, b }) {
+    return a + b;
+}
+
+console.log(addProps(deObj));
+
+//spread
+
+const spreadArr = [23, 423424, 52454, 4244, 42, 4];
+console.log(spreadArr);
+console.log(...spreadArr);
+console.log(spreadArr[0], spreadArr[1]);
+console.log(Math.max(...spreadArr));
+
+const obj1 = {
+    p1: "från objekt 1",
+};
+
+const obj2 = {
+    p2: "från objekt 2",
+    p3: "också från objekt 2",
+};
+
+const obj3 = { ...obj1, ...obj2 };
+console.log(obj3); //Innehåller egenskaperna p1, p2, p3
+
+// spread och klona
+const clonedObj2 = { ...obj2 };
+obj2.p2 = "test";
+console.log(clonedObj2, obj2);
