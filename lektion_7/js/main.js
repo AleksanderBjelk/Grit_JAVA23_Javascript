d5e3a79edd420b689a2e65682268de88133f4539
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-04-16 11:15:16 +0200

    p

---
+++ lektion_7/js/main.js
@@ -0,0 +1,12 @@
+import { getRandomDogImage, getRandomDogImages } from "./dogAPI.js";
+import  displayImage, {displayImages} from "./display.js";
+import * as utils from "./utilities.js"
+
+// getRandomDogImage().then(displayImage);
+//getRandomDogImages(10).then(displayImages);
+getRandomDogImages(10).then((urls) =>{
+const imgUrls = utils.sortDescending(urls);
+displayImages(imgUrls)
+});
+
+
